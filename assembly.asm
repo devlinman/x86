acd1a4288348b9e94426788176df8f7f22dda5b2
devlinman <dev.linman.tech@gmail.com>
2026-05-24 22:02:52 +0530

    The ancient night was torn asunder

--- assembly.asm
+++ assembly.asm
@@ -1,83 +1,82 @@
+; assembly.asm
+; Author: devlinman
+
 global _start
 
 section .data
     kenobi:     db 10, "Hello, there!", 10, 0
     greivous:   db "General kenobi...", 10, 0
-    b_read:     db " bytes read.", 10, 0
-    prompt:     db 10, "Enter number: ", 0
-    answer_text:db " Recieved.", 10, 10, "factorial(", 0
+    prompt:     db 10, "Enter a number to calculate factorial. (20 digits max)", 10, ">> ", 0
+    b_read:     db " bytes recieved.", 10, "You entered: ", 0
+    answer_text:db 10, 10, "factorial(", 0
     equals:     db ") = ", 0
     new_line:   db 10,0
-    error:      db 10, "Invalid string found", 10, 0
+    error:      db 10, 10, "Invalid input.", 10, "Exiting due to error.", 10, 0
 
 section .bss
     buffer: resb 128            ; reserve 128 bytes
 
 section .text
-invalid:
-    xor rbx, rbx
-    lea rdi, [rel error]
-    call println
-    mov rdi, 10
-    jmp done
-
 done:
     mov rax, 60                 ; rax = 60
     syscall                     ; exit(rdi)
 
+invalid:                        ; A panic handler
+    xor rbx, rbx                ; print_string enabled
+    lea rdi, [rel error]        ;
+    call println                ; Print Error message
+    mov rdi, 100                ; exit(100)
+    jmp done                    ; What have I done!
+
 print_dispatch:
     mov rax, 1                  ; write syscall
     mov rdi, 1                  ; fd = 1 = stdout
-    syscall                     ;
+    syscall                     ; assuming rsi = pointer to buffer, rdx = length of buffer
     ret                         ;
 
-print_number:
-    pop rax                     ; rdx:rax / rbx
-    xor rdx, rdx                ; clear rdx - Upper word
-    mov rbx, 10                 ; divisor = 10
-    div rbx                     ;
-    add rcx, 1                  ; length += 1
-    add rdx, 48                 ; ASCII code for '0'
-    push rdx                    ; Store remainder
-    push rax                    ; This will pop off again in next loop
-    cmp rax, 0                  ; if rax != 0
-    jnz print_number            ;       loop
-    imul rcx, 8                 ; 1 byte
-    mov rdx, rcx                ; rdx = length = rcx
-    sub rcx, 8                  ; Because we already start from first word (byte)
-    sub rsi, rcx                ; Point to the first number (Because stack decreases.)
-    mov rsp, rbp                ; restore base
-    xor rbp, rbp                ;
-    pop rbx                     ; restore flag for re-use
-    jmp print_dispatch          ;
-
-print_string:
-    add rdx, 1                  ; index += 1
-    movzx eax, byte [rdi + rdx] ; zero extend and copy byte. Using mov will assume 64-bit.
-    cmp eax, r10d               ; if string[index] != 0 (default) or some delimiter supplied
-    jne print_string            ;       loop
-    mov rsp, rbp                ; restore base
-    xor rbp, rbp                ;
-    pop rbx                     ; restore flag for re-use
-    jmp print_dispatch          ;
-
 println:
+    push rbp                    ; store rbp
+    mov rbp, rsp                ; save rsp
     push rbx                    ; store flag
     xor rax, rax                ; clear rax
     xor rcx, rcx                ; clear rcx
     xor rdx, rdx                ; start with rdx = length = 0
     xor rsi, rsi                ; clear rsi
-    xor rbp, rbp                ; clear rbp
-    mov rbp, rsp                ; store base
     mov rsi, rdi                ; store pointer
     cmp rbx, 0                  ; if flag == 0 :
-    jz print_string             ;    print string
-    push [rdi]                  ; push number onto stack
-    mov rsi, rsp                ; store address of number
-    mov rax, [rsi]              ; store number in rax
-    jmp print_number            ;
+    jz .print_string            ;     print_string
+    push [rdi]                  ; store stack
+    mov rsi, rsp                ; store pointer to number
+    mov rax, [rdi]              ; save number in rax
+    jmp .print_number           ;
+.print_string:
+    add rdx, 1                  ; index += 1
+    movzx eax, byte [rdi + rdx] ; zero extend and copy byte. Using mov will assume 64-bit.
+    cmp eax, r10d               ; if string[index] != 0 (default) or some delimiter supplied
+    jne .print_string           ;     loop
+    pop rbx                     ; restore flag for re-use
+    mov rsp, rbp                ; restore rsp
+    pop rbp                     ; restore rbp
+    jmp print_dispatch          ;
+.print_number:
+    xor rdx, rdx                ; rdx = Upper word = 0
+    mov rbx, 10                 ; divisor = 10
+    div rbx                     ; rdx:rax / rbx --> rdx = reaminder, rax = quotient
+    add rdx, '0'                ; convert remainder to ASCII
+    push rdx                    ; Store remainder
+    add rcx, 1                  ; count += 1
+    cmp rax, 0                  ; if rax (quotient) != 0
+    jnz .print_number           ;     loop
+    imul rcx, 8                 ; length = 1 byte * count
+    mov rdx, rcx                ; rdx = length
+    sub rsi, rcx                ; Point to the first number (Because stack decreases.)
+    pop rbx                     ; restore flag for re-use
+    mov rsp, rbp                ; restore rsp
+    pop rbp                     ; restore rbp
+    jmp print_dispatch          ;
 
-read_line:
+readln:
+    push rbx                    ; store flag
     xor rax, rax                ; read syscall 0
     xor rdi, rdi                ; fd = 0 = sdtin
     syscall                     ; assuming arg1 = rsi = pointer, arg2 = rdx = Max. bytes
@@ -91,8 +90,10 @@ read_line:
     lea rdi, [rel b_read]       ; print "__ number of bytes read.\n"
     call println                ;
     pop rax                     ; return rax = number of bytes read.
-    cmp rax, 0
-    jle invalid
+
+    pop rbx                     ; restore flag
+    cmp rax, 0                  ; if input.is_invalid:
+    jle invalid                 ;     The negotiations _were_ short.
     ret                         ;
 
 _start:
@@ -101,60 +102,57 @@ _start:
     lea rdi, [rel kenobi]       ; arg1 = rdi = &string
     call println                ; rbx = flag (0 for string), arg1 = rdi = *int / *char, arg2 = r10 = delimiter byte
 
-    lea rdi, [rel greivous]     ;
+    lea rdi, [rel greivous]     ; You're shorter than I expected.
     call println                ;
 
-    lea rdi, [rel prompt]
-    call println
+    lea rdi, [rel prompt]       ; Ah, yes. The negotiator.
+    call println                ;
 
     lea rsi, [rel buffer]       ; address of buffer
     mov rdx, 128                ; Max. bytes
-    call read_line              ;
+    call readln                 ; Your move.
     push rax                    ; store number of bytes read
-
 
     lea rdi, [rel buffer]       ; retrieve address of buffer
     mov r10, 10                 ; terminated by LF, delimiter = 10
-    call println                ;
+    call println                ; Execute Order 66.
     xor r10, r10                ; continue printing null-terminated strings
-
 
     lea rdi, [rel buffer]       ; arg1 = &n
     pop rdx                     ; rdx = length (number of bytes read)
-    call convert_buffer_to_number ; arg1 = rdi = pointer, arg2 = rdx = length
-    push rax                      ;
+    call convert_buffer_to_number ; arg1 = rdi = pointer, arg2 = rdx = length, return = rax
+    push rax                    ; <-------------- pushed here
 
-    xor rbx, rbx                ;
+    xor rbx, rbx                ; print_string enabled
     lea rdi, [rel answer_text]  ; print "factorial("
     call println                ;
 
-    mov rbx, 1                  ;
+    mov rbx, 1                  ; print_number enabled
     mov rdi, rsp                ; arg1 = rdi = &n
-    call println                ;
+    call println                ; print n
 
     xor rbx, rbx                ; print_string enabled
     lea rdi, [rel equals]       ; print ")="
     call println                ;
 
-    pop rdi
+    pop rdi                     ; ---------------> popped here
     call factorial              ; arg1 = rdi = n, return = rax
+
     push rax                    ; save factorial(n)
-
     mov rdi, rsp                ; arg1 = &n
-    mov rbx, 1
-    call println
-
+    mov rbx, 1                  ; print_number enabled
+    call println                ; print factorial(n)
 
     xor rbx, rbx                ; print_string enabled
     lea rdi, [rel new_line]     ; print "\n"
     call println                ;
 
     xor rdi, rdi                ; exit(0)
-    jmp done                    ;
+    jmp done                    ; In case I don't see ya: Good afternoon, Good evening, and Good night.
 
 base:
     mov rax, 1                  ; Base-case n = 1
-    ret
+    ret                         ;
 
 factorial:
     cmp rdi, 1                  ; if n <= 1 :
@@ -167,22 +165,25 @@ factorial:
     ret                         ;
 
 convert_buffer_to_number:
-    xor rax, rax
-    xor rcx, rcx
-    sub rdx, 1
+    xor rax, rax                ; result = 0
+    xor rcx, rcx                ; index = 0
+    sub rdx, 1                  ; pointer already at first byte
 .loop:
-    movzx rsi, byte [rdi + rcx]
+    movzx rsi, byte [rdi + rcx] ;  rsi = *(pointer + index)
 
-    cmp rsi, '0'
-    jl invalid
-    cmp rsi, '9'
-    jg invalid
+    cmp rsi, '0'                ;
+    jl invalid                  ; Do or Do not.
+    cmp rsi, '9'                ;
+    jg invalid                  ; There is no try.
 
-    sub rsi, '0'
-    imul rax, 10
-    add rax, rsi
-    add rcx, 1
-    cmp rcx, rdx
-    jl .loop
+    sub rsi, '0'                ; __So uncivilized.__ convert byte to number
+    imul rax, 10                ; result = result * 10
+    add rax, rsi                ; result +=
+    add rcx, 1                  ; index += 1
+    cmp rcx, rdx                ; if index != length: loop
+    jl .loop                    ; I'll try spinning. That's a good trick.
 
-    ret
+    ret                         ;
+
+
+; Made with Doom Emacs 
--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/zsh -f
 set -Eeo pipefail
 
+help() {
+    echo -e "$1 \t\t\tbuild the executable."
+    echo -e "$1 \tb|debug \tbuild with debug symbols."
+    echo -e "$1 \tc|clean \tremove executable & objects."
+    echo -e "$1 \td|dump \t\tobject dump."
+    echo -e "$1 \td|dump i|intel \tobject dump with intel syntax."
+    echo -e "$1 \th|help \t\tprint this help."
+}
+
 dump() {
     if [[ "$1" == "intel" ]] || [[ "$1" == "i" ]]; then
         objdump -d -Mintel exe
@@ -27,16 +36,24 @@ build() {
 }
 
 case $1 in
-"d" | "dump")
-    dump "$2"
+"b" | "debug")
+    debug
     ;;
 "c" | "clean")
     clean
     ;;
-"b"|"debug")
-    debug
+"d" | "dump")
+    dump "$2"
+    ;;
+"h" | "help")
+    help "$0"
+    exit
+    ;;
+"")
+    build
     ;;
 *)
-    build
+    help "$0"
+    exit 1
     ;;
 esac
